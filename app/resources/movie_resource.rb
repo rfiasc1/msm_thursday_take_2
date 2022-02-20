f398cec7b6817bac1760794ebc8df915e4e68e95
anonymous anonymous <regan.fiascone@kellogg.northwestern.edu>
2022-02-20 23:15:02 +0000

    Generate graphiti resources for movies

---
+++ app/resources/movie_resource.rb
@@ -0,0 +1,13 @@
+class MovieResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :director_id, :integer
+  attribute :title, :string
+  attribute :description, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
